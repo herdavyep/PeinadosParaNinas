eec01262a731fe2ec7471df1a0aa2bd1f6ca1822
Heriberto David <herdavyep@hotmail.com>
2017-12-26 21:44:27 -0500

    se agrego firebase al proyecto y se probo creando un usuario

---
+++ PeinadosParaNiñas/ViewControllerLogin.swift
@@ -0,0 +1,35 @@
+//
+//  ViewControllerLogin.swift
+//  Peinados Para Niñas
+//
+//  Created by Heriberto David Yepes on 26/12/17.
+//  Copyright © 2017 Heriberto David Yepes. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerLogin: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
